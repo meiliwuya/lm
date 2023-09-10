9a845bc1277104512fe95e6a174b4f2815a37057
meiliwuya <93051401+meiliwuya@users.noreply.github.com>
2023-09-10 22:49:28 +0800

    Upload by PicGo

---
+++ m2/toubu.js
@@ -0,0 +1,9 @@
+document.writeln("  <b><font style=\"font-size: 14px; color: #666666\"></font></b>");
+document.writeln("  <a href=\"/\">");
+document.writeln("  <img src=\"http://p0.meituan.net/dptrust/52f940b70b8835846d4272122987e47b2780.png\" width=\"50\" height=\"92\">");
+document.writeln("  </a>");
+document.writeln("  <img src=\"http://p0.meituan.net/dptrust/4f14b2e27b0ba415249ae895a0ceab3119911.png\" width=\"200\" height=\"92\">");
+document.writeln("  <a href=\"/dy/\">");
+document.writeln("  <img src=\"http://1.papala.eu.org/2023/202309090933479.png\" width=\"50\" height=\"92\">");
+document.writeln("  </a>");
+document.writeln("<br> ");
